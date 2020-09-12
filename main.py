6ba5c9c5fe11d7c8bbd3f02071d336cdcdc26dc9
Rishabh Tiwari <akchitra99@gmail.com>
2020-09-12 20:40:17 +0000

    minor fix

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ def train(epoch):
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.1f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data[0]))
+                100. * batch_idx / len(train_loader), loss.item()))
 
 def test():
     model.eval()
